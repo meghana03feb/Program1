a3ac080f0c05213f467c37e40caf666b97cc60a1
meghana03feb <andavarapumeghana.25.csm@anits.edu.in>
2026-08-14 00:28:39 +0530

    Add PrimePalindrome class to check prime palindromes

---
+++ Recordprograms/Week2/PrimePalindrome.java
@@ -0,0 +1,36 @@
+package core_java;
+import java.util.Scanner;
+
+public class PrimePalindrome {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+
+        int count = 0;
+
+        for (int i = 1; i <= num; i++) {
+            if (num % i == 0)
+                count++;
+        }
+
+        int temp = num;
+        int rev = 0;
+
+        while (temp > 0) {
+            int rem = temp % 10;
+            rev = rev * 10 + rem;
+            temp = temp / 10;
+        }
+
+        if (count == 2 && num == rev)
+            System.out.println("Prime Palindrome");
+        else
+            System.out.println("Not a Prime Palindrome");
+
+        sc.close();
+    }
+}
