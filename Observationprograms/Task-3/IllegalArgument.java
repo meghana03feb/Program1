3dda47e7d063a1fe421d97ee96f21ea154e36a7f
meghana03feb <andavarapumeghana.25.csm@anits.edu.in>
2026-09-10 20:53:03 +0530

    Create IllegalArgument.java

---
+++ Observationprograms/Task-3/IllegalArgument.java
@@ -0,0 +1,17 @@
+package core_java;
+public class IllegalArgument {
+public static void setAge(int age) {
+if (age < 0) {
+throw new IllegalArgumentException("Age cannot be negative.");
+}
+}
+
+public static void main(String[] args) {  
+    try {  
+        setAge(-5);   
+    } catch (IllegalArgumentException e) {  
+        System.err.println("Caught IllegalArgumentException: " + e.getMessage());  
+    }  
+}
+
+}
