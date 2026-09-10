0ddbb5fafdd431b01d46d20654bbbf94ba87b089
meghana03feb <andavarapumeghana.25.csm@anits.edu.in>
2026-09-10 23:51:50 +0530

    Add custom exception for voting eligibility check

---
+++ Observationprograms/Task-3/CustomException.java
@@ -0,0 +1,23 @@
+package core_java;
+class InvalidAgeException extends Exception {
+public InvalidAgeException(String message) {
+super(message);
+}
+}
+
+public class CustomException {
+public static void checkVotingEligibility(int age) throws InvalidAgeException {
+if (age < 18) {
+throw new InvalidAgeException("Must be 18 or older to vote.");
+}
+}
+
+public static void main(String[] args) {  
+    try {  
+        checkVotingEligibility(16);  
+    } catch (InvalidAgeException e) {  
+        System.err.println("Caught Custom Exception: " + e.getMessage());  
+    }  
+}
+
+}
